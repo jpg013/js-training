34c2bbebea8c159a8fabb87e170e885a8c317177
jpg1218 <jgraberdev@gmail.com>
2015-08-20 15:43:35 -0500

    changes

--- node/examples/example1.js
+++ node/examples/example1.js
@@ -1,13 +1,15 @@
 var fs = require('fs');
 var readableStream = fs.createReadStream('example-1-text');
 var data = "";
+
 function parseChunk(chunk) {
   data += chunk;
 }
-function onFinished() { console.log(data) }
+
+function onFinished() {
+  console.log(data)
+}
 
 readableStream.on('data', parseChunk);
-
 readableStream.on("end", onFinished);
-
 console.log("end of file line\n-------------------");
